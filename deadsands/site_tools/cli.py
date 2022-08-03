3488c6f5989a041a2fd32a8921a82346f6dd7812
evilchili <evilchili@gmail.com>
2022-08-02 22:30:15 -0700

    remove invoke dependency, move cli to typer

--- deadsands/site_tools/cli.py
+++ deadsands/site_tools/cli.py
@@ -1,14 +1,41 @@
 import click
+import os
+import shutil
+import subprocess
 import sys
 import typer
+import webbrowser
 
 from enum import Enum
-from site_tools.tasks import CONFIG
+from livereload import Server
 from site_tools.content_manager import create
 
-CONFIG.update(
-    templates_path='markdown-templates',
-)
+from pelican import main as pelican_main
+from pelican.settings import DEFAULT_CONFIG, get_settings_from_file
+
+OPEN_BROWSER_ON_SERVE = True
+SETTINGS_FILE_BASE = 'pelicanconf.py'
+SETTINGS = {}
+SETTINGS.update(DEFAULT_CONFIG)
+LOCAL_SETTINGS = get_settings_from_file(SETTINGS_FILE_BASE)
+SETTINGS.update(LOCAL_SETTINGS)
+
+CONFIG = {
+    'settings_base': SETTINGS_FILE_BASE,
+    'settings_publish': 'publishconf.py',
+    # Output path. Can be absolute or relative to tasks.py. Default: 'output'
+    'deploy_path': SETTINGS['OUTPUT_PATH'],
+    # Remote server configuration
+    'ssh_user': 'greg',
+    'ssh_host': 'froghat.club',
+    'ssh_port': '22',
+    'ssh_path': '/usr/local/deploy/deadsands/',
+    # Host and port for `serve`
+    'host': 'localhost',
+    'port': 8000,
+    # content manager config
+    'templates_path': 'markdown-templates',
+}
 
 app = typer.Typer()
 
@@ -19,6 +46,75 @@ class ContentType(str, Enum):
     monster = 'monster'
     region = 'region'
     page = 'page'
+
+
+def pelican_run(cmd: list = [], publish=False) -> None:
+    settings = CONFIG['settings_publish' if publish else 'settings_base']
+    pelican_main(['-s', settings] + cmd)
+
+
+@app.command()
+def clean() -> None:
+    if os.path.isdir(CONFIG['deploy_path']):
+        shutil.rmtree(CONFIG['deploy_path'])
+        os.makedirs(CONFIG['deploy_path'])
+
+
+@app.command()
+def build() -> None:
+    pelican_run()
+
+
+@app.command()
+def serve() -> None:
+
+    url = 'http://{host}:{port}/'.format(**CONFIG)
+
+    def cached_build():
+        pelican_run(['-e', 'CACHE_CONTENT=true', 'LOAD_CONTENT_CACHE=true',
+                     'SHOW_DRAFTS=true', f'SITEURL="{url}"'])
+
+    clean()
+    cached_build()
+    server = Server()
+    theme_path = SETTINGS['THEME']
+    watched_globs = [
+        CONFIG['settings_base'],
+        '{}/templates/**/*.html'.format(theme_path),
+    ]
+
+    content_file_extensions = ['.md', '.rst']
+    for extension in content_file_extensions:
+        content_glob = '{0}/**/*{1}'.format(SETTINGS['PATH'], extension)
+        watched_globs.append(content_glob)
+
+    static_file_extensions = ['.css', '.js']
+    for extension in static_file_extensions:
+        static_file_glob = '{0}/static/**/*{1}'.format(theme_path, extension)
+        watched_globs.append(static_file_glob)
+
+    for glob in watched_globs:
+        server.watch(glob, cached_build)
+
+    if OPEN_BROWSER_ON_SERVE:
+        webbrowser.open(url)
+
+    server.serve(host=CONFIG['host'], port=CONFIG['port'],
+                 root=CONFIG['deploy_path'])
+
+
+@app.command()
+def publish():
+    clean()
+    pelican_run(publish=True)
+    subprocess.call(
+        'rsync --delete --exclude ".DS_Store" -pthrvz -c '
+        '-e "ssh -p {ssh_port}" '
+        '{} {ssh_user}@{ssh_host}:{ssh_path}'.format(
+            CONFIG['deploy_path'].rstrip('/') + '/',
+            **CONFIG
+        )
+    )
 
 
 @app.command()
